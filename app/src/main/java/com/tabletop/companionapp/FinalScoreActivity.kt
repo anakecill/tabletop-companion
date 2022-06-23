833eaa5b329e10fee186684a004d7fed0b6091d5
anakecill <104648280+anakecill@users.noreply.github.com>
2022-06-23 18:11:19 +0700

    first commit

---
+++ app/src/main/java/com/tabletop/companionapp/FinalScoreActivity.kt
@@ -0,0 +1,11 @@
+package com.tabletop.companionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinalScoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final_score)
+    }
+}
---
+++ app/src/main/java/com/tabletop/companionapp/PlayerActionActivity.kt
@@ -0,0 +1,11 @@
+package com.tabletop.companionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerActionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_action)
+    }
+}
